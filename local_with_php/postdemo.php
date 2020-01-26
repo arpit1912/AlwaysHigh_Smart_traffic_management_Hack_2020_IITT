548f85267abab16f39d975ca716e447534dbab9b
arpit1912 <arpitbandejiya@gmail.com>
2020-01-26 09:25:57 +0530

    seventh commit

---
+++ local_with_php/postdemo.php
@@ -0,0 +1,53 @@
+<?php
+//Creates new record as per request
+    //Connect to database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "espdemo";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Database Connection failed: " . $conn->connect_error);
+        echo "error1";
+    }
+
+    //Get current date and time
+    date_default_timezone_set('Asia/Kolkata');
+    $d = date("Y-m-d");
+    //echo " Date:".$d."<BR>";
+    $t = date("H:i:s");
+
+    if(!empty($_POST['status']) && !empty($_POST['station']))
+    {
+    	$status = $_POST['status'];
+    	$station = $_POST['station'];
+
+	    $sql = "INSERT INTO logs (station, status, Date, Time)
+		
+		VALUES ('".$station."', '".$status."', '".$d."', '".$t."')";
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "OK";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+    else
+    {
+        echo "err";
+        $sql = "INSERT INTO logs (station, status, Date, Time)
+        
+        VALUES ('Arpit', 'hee', '.hee', 'hee')";
+        if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+    }
+
+
+	$conn->close();
+?>
